f2ac7690ccd8df773b78fd2eb20ca77070a8313c
clinton <clintonletsoela724c@gmail.com>
2023-01-14 18:02:24 +0200

    POrtfolio changes

--- pages/index.js
+++ pages/index.js
@@ -15,24 +15,47 @@ export default function Home() {
           <img src='../facial.jpg'/>
           <br></br>
           <br></br>
-          <h2>About</h2>
+          <h2>About Me</h2>
+          <br></br>
           <p className={styles.description.p}>
-            I am a recent Information (Electrical) Engineering graduate from the University of the Witwatersrand.
-            I am passionate about problem solving, particularly in software development. 
+            My name is Ntepela Clinton Letsoela and I am 24 years old. I am a recent Information (Electrical) Engineering graduate from the University of the Witwatersrand.
+            I am passionate about problem solving, particularly in software development. I was born in the Free State province and I moved to Johannesburg when I was 7 years old. 
+            I matriculated in 2016 and decided to do Electrical Engineering because I wanted to solve the electricity crisis in South Africa. I failed my first year at university 
+            because I found it difficult to adapt especially in the first semester.
+            When I got to second year I was introduced to software development and I fell in love with it. When I got to third year I had to repeat again because it was the year
+            when online learning was introduced due to the COVID-19 pandemic and we had to vacate from our student residences. Online learning and being at home made it difficult
+            for me to study and these are some of the reasons I had to repeat third year. Furthermore, my love for software infludenced me to branch to
+            Information Engineering in third year when we were presented with an option to branch. Last year (2022), I finally completed my degree and this made me happy and boosted my confidence.
+            I am now looking for a software company that will enable me to enhance the skills that I currently posses because I want grow in the software industry.
           </p>
           <br></br>
-          <h2>Why I should join SovTech</h2>
+          <h2>Why I should join SovTech!!</h2>
+          <br></br>
           <p>
             I believe that I have the necessary skills required to join the software engineering graduate program at SovTech.
             Furthermore, the skills that I possess at the moment are still limited and I may not contribute directly to the success of the
             company but I am willing to learn and improve. In addition I am willing to dedicate myself to the work that will be given to me so 
             that next year I can be a suitable candidate for full time employment at SovTech. In addition to my technical skills, I am an effecient
-            team worker and I have good communication skills.
+            team worker and I have good communication skills. 
           </p>
           <br></br>
           <h2>Previous Projects</h2>
+              <br></br>
+              <p><strong>1. Web Scraping System for Big Data Applications: </strong>This was my final year project. We had to create a web scraping system that 
+                consists of two components; a web scraper and a machine learning algorithm to analyse the scraped data. This system was developed using  Link:
+                <a  href="https://github.com/clinton724/ELEN4012-Web_Srapping" className={styles.link} target="_blank"><u>https://github.com/clinton724/ELEN4012-Web_Srapping</u></a>
+              </p>
+              <br></br>
+              <p><strong>2. Small React app: </strong>This is a small web app where I was introducing myself to react. It is a simple app that consists of a signup page
+              and validates user information before signing in and hashes the password. Link: <a href="https://github.com/clinton724/Software-webApp" className={styles.link} target="_blank"> <u>https://github.com/clinton724/Software-webApp</u></a></p>
+              <br></br>
+              <p><strong>3. Learning Management System: </strong>This was a 4th year software development project that was introducing web development. Link: <a href="https://drive.google.com/drive/folders/1RODp-KFcb73IX4iQ5xoN0QAO6wRZseZZ?usp=share_link" className={styles.link} target="_blank"> <u>https://drive.google.com/drive/folders/1RODp-KFcb73IX4iQ5xoN0QAO6wRZseZZ?usp=share_link</u></a></p>
           <br></br>
           <h2>Recommendations</h2>
+          <br></br>
+          <p>I was part of a data science intership last year. The internship was 6 weeks, it started in the July study break and ended in December. During the June-July period, the course covers data analysis techniques. There is a brief introduction to AWS, data warehousing, business intelligence and ETL concepts. The final period of the course covered machine learning and artificial intelligence in cloud computing. We Worked with Amazon Lambda, Lex, Kendra, S3, Comprehend and Sage.
+            Suraksha Motilal is one of the students I was working with and the screenshot below shows what she had to say about me.
+          </p>
           <br></br>
           <img src='../Recommendations.png'/>
           <p> For more information, please visit my linkedIn profile<a href="https://www.linkedin.com/in/clinton-letsoela-713654153/" className={styles.link} target="_blank"> <u>here</u></a>.</p>
