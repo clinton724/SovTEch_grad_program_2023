963ba70db2e15468bd74667becce09685ac3eec5
clinton <clintonletsoela724c@gmail.com>
2023-01-16 11:03:04 +0200

    final code

--- pages/index.js
+++ pages/index.js
@@ -24,9 +24,9 @@ export default function Home() {
             because I found it difficult to adapt especially in the first semester.
             When I got to second year I was introduced to software development and I fell in love with it. When I got to third year I had to repeat again because it was the year
             when online learning was introduced due to the COVID-19 pandemic and we had to vacate from our student residences. Online learning and being at home made it difficult
-            for me to study and these are some of the reasons I had to repeat third year. Furthermore, my love for software infludenced me to branch to
+            for me to study and these are some of the reasons I had to repeat third year. Furthermore, my love for software influenced me to branch to
             Information Engineering in third year when we were presented with an option to branch. Last year (2022), I finally completed my degree and this made me happy and boosted my confidence.
-            I am now looking for a software company that will enable me to enhance the skills that I currently posses because I want grow in the software industry.
+            I am now looking for a software company that will enable me to enhance the skills that I currently possess because I want grow in the software industry.
           </p>
           <br></br>
           <h2>Why I should join SovTech!!</h2>
@@ -35,10 +35,10 @@ export default function Home() {
             I believe that I have the necessary skills required to join the software engineering graduate program at SovTech.
             Furthermore, the skills that I possess at the moment are still limited and I may not contribute directly to the success of the
             company but I am willing to learn and improve. In addition I am willing to dedicate myself to the work that will be given to me so 
-            that next year I can be a suitable candidate for full time employment at SovTech. In addition to my technical skills, I am an effecient
+            that next year I can be a suitable candidate for full time employment at SovTech. In addition to my technical skills, I am an efficient
             team worker and I have good communication skills. I have worked on a few projects independently and with team members. Working alone has boosted
             my confidence and has enabled me to have the ability to understand concepts individually and this has made me to be a self-driven person. Working
-            with people has made me appreciate the importance of team work and good communication skills.
+            with people has made me appreciate the importance of team work and good communication skills. 
           </p>
           <br></br>
           <h2>Previous Projects</h2>
@@ -62,7 +62,7 @@ export default function Home() {
           <br></br>
           <h2>Recommendations</h2>
           <br></br>
-          <p>I was part of a data science intership last year. The internship was 6 weeks, it started in the July study break and ended in December. During the June-July period, the course covers data analysis techniques. There is a brief introduction to AWS, data warehousing, business intelligence and ETL concepts. The final period of the course covered machine learning and artificial intelligence in cloud computing. We Worked with Amazon Lambda, Lex, Kendra, S3, Comprehend and Sage.
+          <p>I was part of a data science intership last year. The internship was 6 weeks, it started in the July study break and ended in December. During the June-July period, the course covers data analysis techniques. There is a brief introduction to AWS, data warehousing, business intelligence and ETL concepts. The final period of the course covered machine learning and artificial intelligence in cloud computing. We worked with Amazon Lambda, Lex, Kendra, S3, Comprehend and Sage.
             Suraksha Motilal is one of the students I was working with and the screenshot below shows what she had to say about me.
           </p>
           <br></br>
